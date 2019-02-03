8486df46e8517ad4457d0d1a798380414bce91b8
Sergey Mamulchenko <hmnzr@yahoo.com>
2019-02-03 11:55:34 +0700

    pipeline-game implementation

---
+++ Assets/Scripts/Pipeline/GiftSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftSpawn : MonoBehaviour
+{
+    private Random random;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnGift());
+    }
+
+    IEnumerator SpawnGift()
+    {
+        var timeout = Random.Range(0.6f, 1.8f);
+        yield return new WaitForSeconds(timeout);
+
+        CreateGift();
+    }
+
+    void CreateGift()
+    {
+        var color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        GetComponent<Renderer>().material.color = color;
+    }
+    
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Pipeline/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private int _grabbedGifts = 0;
+
+    public Text CounterLabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CounterLabel.text = _grabbedGifts.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            var myCollider = GetComponent<BoxCollider>();
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 5000f))
+            {
+                if (hit.collider.gameObject.tag == "Gift")
+                {
+                    Destroy(hit.collider.gameObject);
+                    _grabbedGifts++;
+                }
+            }
+        }
+    }
+}
