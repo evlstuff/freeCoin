ccc597b430b752ec40fbae06673f4f5cbdfdb859
shamakds <evl.game.stuff@gmail.com>
2019-02-03 12:13:06 +0700

    Update move controller

--- Assets/Scripts/Common/MoveCtrl.cs
+++ Assets/Scripts/Common/MoveCtrl.cs
@@ -2,13 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum MoveTypes
+{
+    Direction, JumpToPosition, Custom
+}
+
 public class MoveCtrl : MonoBehaviour
 {
-    public Vector3 Direction = Vector3.left;
+    [Header("General")]
+    public MoveTypes MoveType = MoveTypes.Direction;
     public float Speed = 2f;
+    public float JumpForce = 2f;
+
+    [Header("Direction Props")]
+    public Vector3 Direction = Vector3.left;
+
+    [Header("Jump To Position Props")]
+    public Transform JumpTargetTransform;
+    public Vector3 JumpDirection;
+
+    void UodateDirectionalMovement()
+    {
+        transform.position += Direction * Speed * Time.deltaTime;
+    }
+
+    void UpdateJumpToPositionMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            //JumpDirection = Vector3.MoveTowards()
+        }
+    }
 
     void FixedUpdate()
     {
-        transform.position += Direction * Speed * Time.deltaTime;
+        switch(MoveType)
+        {
+            case MoveTypes.Direction:
+                {
+                    UodateDirectionalMovement();
+                    break;
+                }
+            case MoveTypes.JumpToPosition:
+                {
+                    UpdateJumpToPositionMovement();
+                    break;
+                }
+        }
     }
 }
